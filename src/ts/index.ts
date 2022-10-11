00211d30d8dd4b9aa59c3665001df2cca3ac6508
Victor Redondo <vrviktor@gmail.com>
2022-10-11 23:42:44 +0200

    Add array of characters

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,1 +1,18 @@
-const a = 3;
+import Advisor from "./characters/Advisor/Advisor";
+import Fighter from "./characters/Fighter/Fighter";
+import King from "./characters/King/King";
+import Squire from "./characters/Squire/Squire";
+
+const jaime = new Fighter("Jaime", "Lannister", 40, "sword", 9);
+
+const joffrey = new King("Joffrey", "Lannister", 16, 1);
+
+const daenerys = new Fighter("Daenerys", "Targaryen", 25, "Dragons", 5);
+
+const tyrion = new Advisor("Tyrion", "Lannister", 30, daenerys);
+
+const bronn = new Squire("Bronn", "Blackwater", 45, jaime, 3);
+
+const characters = [joffrey, jaime, daenerys, tyrion, bronn];
+
+export default characters;
